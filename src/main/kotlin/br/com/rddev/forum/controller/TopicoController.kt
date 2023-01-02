fac7269d750c47afa407c58d22b3bfbc723a17ef
Raphael Fontoura <raphael.fontoura@gmail.com>
2023-01-02 16:44:19 -0300

    Refactor REST

--- src/main/kotlin/br/com/rddev/forum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/rddev/forum/controller/TopicoController.kt
@@ -4,13 +4,19 @@ import br.com.rddev.forum.dto.AtualizacaoTopicoForm
 import br.com.rddev.forum.service.TopicoService
 import br.com.rddev.forum.dto.NovoTopicoForm
 import br.com.rddev.forum.dto.TopicoView
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.util.UriBuilder
+import org.springframework.web.util.UriComponentsBuilder
 import javax.validation.Valid
 
 @RestController
@@ -28,12 +34,26 @@ class TopicoController(private val service: TopicoService) {
     }
 
     @PostMapping
-    fun cadastrar(@RequestBody @Valid form: NovoTopicoForm) {
-        service.cadastrar(form)
+    fun cadastrar(
+        @RequestBody @Valid form: NovoTopicoForm,
+        uriBuilder: UriComponentsBuilder
+    ): ResponseEntity<TopicoView> {
+        val topicoView = service.cadastrar(form)
+        val uri = uriBuilder.path("/topicos/${topicoView.id}").build().toUri()
+        return ResponseEntity.created(uri).body(topicoView)
     }
 
     @PutMapping
-    fun atualizar(@RequestBody @Valid form: AtualizacaoTopicoForm) {
-        service.atualizar(form)
+    fun atualizar(
+        @RequestBody @Valid form: AtualizacaoTopicoForm
+    ): ResponseEntity<TopicoView> {
+        val topicoView = service.atualizar(form)
+        return ResponseEntity.ok(topicoView)
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun deletar(@PathVariable id: Long) {
+        service.deletar(id)
     }
 }
