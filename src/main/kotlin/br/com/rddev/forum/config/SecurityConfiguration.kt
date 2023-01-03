631e166b737a0febbe7dd08b6b4c8b6fd7b730cc
Raphael Fontoura <raphael.fontoura@gmail.com>
2023-01-03 16:20:50 -0300

    Create profiles in Maven and configure application properties

--- src/main/kotlin/br/com/rddev/forum/config/SecurityConfiguration.kt
+++ src/main/kotlin/br/com/rddev/forum/config/SecurityConfiguration.kt
@@ -13,7 +13,6 @@ import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-import org.springframework.web.filter.OncePerRequestFilter
 
 @Configuration
 @EnableWebSecurity
