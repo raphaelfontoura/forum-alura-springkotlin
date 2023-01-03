1b5d260b55d6120eb637a1ed0f31655f826cc3db
Raphael Fontoura <raphael.fontoura@gmail.com>
2023-01-02 23:49:58 -0300

    Add PageableDefault

--- src/main/kotlin/br/com/rddev/forum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/rddev/forum/controller/TopicoController.kt
@@ -6,6 +6,7 @@ import br.com.rddev.forum.dto.NovoTopicoForm
 import br.com.rddev.forum.dto.TopicoView
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.transaction.annotation.Transactional
@@ -30,7 +31,7 @@ class TopicoController(private val service: TopicoService) {
     @GetMapping
     fun listar(
         @RequestParam(required = false) nomeCurso: String?,
-        paginacao: Pageable
+        @PageableDefault(size = 5) paginacao: Pageable
     ): Page<TopicoView> {
         return service.listar(nomeCurso, paginacao)
     }
