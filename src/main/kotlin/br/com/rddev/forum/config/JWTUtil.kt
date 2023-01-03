6e1ad1255a0d1eb8f14ac1b6c0cf5e07c83c8ac9
Raphael Fontoura <raphael.fontoura@gmail.com>
2023-01-03 12:21:44 -0300

    Add roles authorities to jwt

--- src/main/kotlin/br/com/rddev/forum/config/JWTUtil.kt
+++ src/main/kotlin/br/com/rddev/forum/config/JWTUtil.kt
@@ -1,24 +1,29 @@
 package br.com.rddev.forum.config
 
+import br.com.rddev.forum.service.UsuarioService
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
+import org.springframework.security.core.GrantedAuthority
 import org.springframework.stereotype.Component
 import java.util.*
 
 @Component
-class JWTUtil {
+class JWTUtil(
+    private val usuarioService: UsuarioService
+) {
 
     private val expiration : Long = 60000
 
     @Value("\${jwt.secret}")
     private lateinit var secret: String
 
-    fun generateToken(username: String) : String? {
+    fun generateToken(username: String, authorities: MutableCollection<out GrantedAuthority>) : String? {
         return Jwts.builder()
             .setSubject(username)
+            .claim("role", authorities)
             .setExpiration(Date(System.currentTimeMillis() + expiration))
             .signWith(SignatureAlgorithm.HS512, secret.toByteArray())
             .compact()
@@ -35,6 +40,7 @@ class JWTUtil {
 
     fun getAuthentication(jwt: String?): Authentication {
         val username = Jwts.parser().setSigningKey(secret.toByteArray()).parseClaimsJws(jwt).body.subject
-        return UsernamePasswordAuthenticationToken(username, null, null)
+        val authorities = usuarioService.loadUserByUsername(username).authorities
+        return UsernamePasswordAuthenticationToken(username, null, authorities)
     }
 }
