df9ae05424309df57c4124ee36e6699f458ea5b2
Raphael Fontoura <raphael.fontoura@gmail.com>
2023-01-04 18:02:25 -0300

    Add test to exception and get by id with optional

--- src/test/kotlin/br/com/rddev/forum/service/TopicoServiceTest.kt
+++ src/test/kotlin/br/com/rddev/forum/service/TopicoServiceTest.kt
@@ -1,5 +1,6 @@
 package br.com.rddev.forum.service
 
+import br.com.rddev.forum.exception.NotFoundException
 import br.com.rddev.forum.mapper.TopicoFormMapper
 import br.com.rddev.forum.mapper.TopicoViewMapper
 import br.com.rddev.forum.model.TopicoTest
@@ -9,8 +10,11 @@ import io.mockk.every
 import org.junit.jupiter.api.Test
 import io.mockk.mockk
 import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.assertThrows
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
+import java.util.*
 
 class TopicoServiceTest {
 
@@ -20,8 +24,11 @@ class TopicoServiceTest {
 
     val topicoRepository: TopicoRepository = mockk {
         every { findByCursoNome(any(), any()) } returns topicos
+        every { findAll(paginacao) } returns topicos
     }
-    val topicoViewMapper: TopicoViewMapper = mockk()
+    val topicoViewMapper: TopicoViewMapper = mockk {
+        every { map(any()) } returns TopicoViewTest.build()
+    }
     val topicoFormMapper: TopicoFormMapper = mockk()
 
     val topicoService = TopicoService(
@@ -30,12 +37,41 @@ class TopicoServiceTest {
 
     @Test
     fun `deve listar topicos a partir do nome do curso`() {
-        every { topicoViewMapper.map(any()) } returns TopicoViewTest.build()
 
         topicoService.listar("Kotlin avancado", paginacao)
 
         verify(exactly = 1) { topicoRepository.findByCursoNome(any(), any()) }
         verify(exactly = 1) { topicoViewMapper.map(any()) }
         verify(exactly = 0) { topicoRepository.findAll(paginacao) }
+    }
+
+    @Test
+    fun `deve listar todos os topicos quando o nome do curso for nulo`() {
+        topicoService.listar(null, paginacao)
+
+        verify(exactly = 0) { topicoRepository.findByCursoNome(any(), any()) }
+        verify(exactly = 1) { topicoViewMapper.map(any()) }
+        verify(exactly = 1) { topicoRepository.findAll(paginacao) }
+    }
+
+    @Test
+    fun `deve retornar NotFoundException quando topico nao for achado`() {
+        every { topicoRepository.findById(99L) } returns Optional.empty()
+
+        val result = assertThrows<NotFoundException> {
+            topicoService.buscarPorId(99L)
+        }
+
+        assertThat(result.message).isEqualTo("Tópico não encontrado")
+    }
+
+    @Test
+    fun `deve retornar um Topico quando topico for achado`() {
+        val topico = TopicoTest.build()
+        every { topicoRepository.findById(1L) } returns Optional.of(topico)
+
+        val result = topicoService.buscarPorId(1L)
+
+        assertThat(result.titulo).isEqualTo(topico.titulo)
     }
 }
